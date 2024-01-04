9d7a7166208f2dae9c127229021c1b8458edcfa8
Meteorych <super.titlov@inbox.ru>
2024-01-04 21:05:16 +0300

    Layout js project

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("I don't like pizza!");
+
+let number = 5;
+console.log(number);
+let username = window.prompt("Enter your username: ");
+window.alert(username);
+
+document.getElementById("p1").innerHTML = "My number is " + number;
